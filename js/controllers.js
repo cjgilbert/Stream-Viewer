b5e75e23bad30c61bf566f77d9f23f261dc62ebe
Chris Gilbert <chris@cgilbert.net>
2014-03-02 15:50:17 -0800

    New features of choosing which games to display.

--- js/controllers.js
+++ js/controllers.js
@@ -4,9 +4,46 @@ var streamViewer = angular.module('StreamViewer', ['streamViewer.services'])
     }
 ]);
 
-streamViewer.controller('StreamListCtrl', ['$scope', 'twitch', 'storage', function($scope, twitch, storage) {
+streamViewer.controller('StreamListCtrl', ['$scope', 'twitch', 'storage', '$timeout', function($scope, twitch, storage, $timeout) {
     $scope.getLogo = function(game) {
+        console.log($scope.logos);
         return $scope.logos[game];
+    };
+
+    $scope.saveGames = function() {
+        storage.save('games', $scope.games);
+        $scope.loadStreams();
+        $scope.settings = false;
+    };
+
+    $scope.loadGames = function() {
+        storage.load('games', function(result) {
+            $scope.games = result.games;
+            $scope.loadStreams();
+        });
+    };
+
+    $scope.toggleGame = function(val) {
+        var index = $scope.games.indexOf(val);
+        if (-1 == index) {
+            $scope.games.push(val);
+        } else {
+            $scope.games.splice(index, 1);
+        }
+    };
+
+    $scope.loadStreams = function() {
+        if (0 < $scope.games.length) {
+            twitch.getStreams($scope.games, $scope.limit, function(response) {
+                $scope.streams = response;
+                $scope.loadFavorites();
+            });
+        } else {
+            $scope.streams = [];
+            $scope.$apply(function() {
+                $scope.settings = true;
+            });
+        }
     };
 
     $scope.loadFavorites = function() {
@@ -39,14 +76,37 @@ streamViewer.controller('StreamListCtrl', ['$scope', 'twitch', 'storage', functi
         storage.save('favorites', $scope.favorites);
     };
 
-    $scope.logos = {};
-    $scope.favorites = {};
-    $scope.logos['World of Warcraft: Mists of Pandaria'] = "img/icon/wow-icon.png";
-    $scope.logos['League of Legends'] = "img/icon/league.png";
-    $scope.games = ['League of Legends', 'World of Warcraft: Mists of Pandaria'];
-    twitch.getStreams($scope.games, function(response) {
-        $scope.streams = response;
-        $scope.loadFavorites();
-    });
+    $scope.toggleSettings = function () {
+        // Unfortunate hack to allow Chrome extension to update the DOM.
+        $scope.width = 399;
+        $timeout(function() {
+            $scope.width = 400;
+        }, 5);
+        $scope.settings = !$scope.settings;
+    };
 
+    $scope.logos = {};
+    $scope.games = {};
+    $scope.availableGames = [
+        {'name': 'Call of Duty: Ghosts'},
+        {'name': 'Diablo 3'},
+        {'name': 'Hearthstone: Heroes of Warcraft'},
+        {'name': 'League of Legends'},
+        {'name': 'StarCraft II: Heart of the Swarm'},
+        {'name': 'The Elder Scrolls Online'},
+        {'name': 'World of Warcraft: Mists of Pandaria'}
+    ];
+    $scope.favorites = {};
+    $scope.settings = false;
+    $scope.width = 400;
+    $scope.limit = 50;
+    $scope.logos['Call of Duty: Ghosts'] = 'img/icon/cod-icon.png';
+    $scope.logos['Diablo 3'] = 'img/icon/diablo-icon.png';
+    $scope.logos['Hearthstone: Heroes of Warcraft'] = 'img/icon/hearthstone-icon.png';
+    $scope.logos['League of Legends'] = 'img/icon/league-icon.png';
+    $scope.logos['StarCraft II: Heart of the Swarm'] = 'img/icon/hots-icon.png';
+    $scope.logos['The Elder Scrolls Online'] = 'img/icon/elderscrolls-icon.png';
+    $scope.logos['World of Warcraft: Mists of Pandaria'] = '/img/icon/wow-icon.png';
+    $scope.games = [];
+    $scope.loadGames();
 }]);
--- js/services.js
+++ js/services.js
@@ -1,10 +1,11 @@
 angular.module('streamViewer.services', [], function($provide) {
     $provide.factory('twitch', ['$http', function($http){
         return {
-            getStreams: function(games, callback) {
+            getStreams: function(games, limit, callback) {
                 var streams = [];
                 for (var i = 0; i < games.length; i++) {
-                    $http.get('https://api.twitch.tv/kraken/streams?game='+games[i])
+                    var gameLimit = limit/games.length;
+                    $http.get('https://api.twitch.tv/kraken/streams?game='+games[i]+'&limit='+gameLimit)
                         .success(function(response) {
                             for (var j = 0; j < response.streams.length; j++) {
                                 streams.push({'name': response.streams[j].channel.name,
@@ -25,12 +26,10 @@ angular.module('streamViewer.services', [], function($provide) {
             save: function(key, value) {
                 var obj = {};
                 obj[key] = value;
-                chrome.storage.sync.clear();
                 chrome.storage.sync.set(obj);
             },
             load: function(key, callback) {
                 chrome.storage.sync.get(key, function(result){
-                    console.log(result);
                     callback(result);
                 });
             }
